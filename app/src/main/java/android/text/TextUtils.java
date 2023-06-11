8bdd62ec93d96779d5fc6f8e5ebe61f27407f2d2
zzz <eg79@qq.com>
2023-06-12 00:03:32 +0800

    更新 TextUtils 类

--- app/src/main/java/android/text/TextUtils.java
+++ app/src/main/java/android/text/TextUtils.java
@@ -1,29 +1,18 @@
 package android.text;
 
-import java.util.ArrayList;
+import java.util.Iterator;
 
 public class TextUtils {
-    public static boolean isEmpty(CharSequence str) {
-        if (str == null || str.length() == 0)
-            return true;
-        else
-            return false;
-    }
-
-    public static <E extends Object> String join(CharSequence delimiter, ArrayList<E> array) {
-        Object[] tokens = new Object[array.size()];
-        for (int i = 0; i < array.size(); i++) {
-            tokens[i] = array.get(i);
+    public static String join(CharSequence delimiter, Iterable tokens) {
+        final Iterator<?> it = tokens.iterator();
+        if (!it.hasNext()) {
+            return "";
         }
-        StringBuilder sb = new StringBuilder();
-        boolean firstTime = true;
-        for (Object token : tokens) {
-            if (firstTime) {
-                firstTime = false;
-            } else {
-                sb.append(delimiter);
-            }
-            sb.append(token);
+        final StringBuilder sb = new StringBuilder();
+        sb.append(it.next());
+        while (it.hasNext()) {
+            sb.append(delimiter);
+            sb.append(it.next());
         }
         return sb.toString();
     }
